f21705b2557ebe7c31cca39a7891393154dd28cd
bears <bears@bears.zone>
2022-09-07 15:48:56 -0500

    canonical data-import cleanup pursuant to #2 and removal of cache pursuant to #1

--- canonical-data-import.ts
+++ canonical-data-import.ts
@@ -8,45 +8,33 @@ import redis from 'redis'
 main()
 
 async function main() {
-    const bulk_data_req = await fetch('https://api.scryfall.com/bulk-data', { cachePath: './cache', cache: 'force-cache' })
+    const bulk_data_req = await fetch('https://api.scryfall.com/bulk-data')
 
     const bulk_data = await bulk_data_req.json()
+
+    // TODO: Catch errors
     const bulk_data_uri = bulk_data['data'][3]['download_uri']
 
-    const all_cards_request = await fetch(bulk_data_uri, { cachePath: './cache', cache: 'force-cache' })
+    var batchSize = 1000
 
-    // const pipeline = all_cards_request.body.pipe(streamarray.withParser());
+    const all_cards_request = await fetch(bulk_data_uri)
+    const all_cards_stream = chain.chain([all_cards_request.body.pipe(streamarray.withParser()), new Batch({ batchSize: batchSize })])
 
-    const pipeline = chain.chain([all_cards_request.body.pipe(streamarray.withParser()), new Batch({ batchSize: 1000 })])
-
-    // const pipeline = chain([ fs.createReadStream('sample.json'), StreamArray.withParser(), new Batch({batchSize: 100}) ]);
     const client = redis.createClient({});
     await client.connect().then(() => {
         client.on('connect', () => console.log('connected to redis successfully!'));
         client.on('error', (err) => console.log('Redis Client Error', err));
     });
 
-    // await client.set('key', 'value');
+    var current_key = 0
 
-    var batchSize = 0
-
-    pipeline.on('data', data => {
-        console.log(batchSize + ' processing ' + data[0]['value']['id'])
+    all_cards_stream.on('data', data => {
+        console.log(current_key + ' processing ' + data[0]['value']['id'])
         data.forEach(async element => {
             await client.json.set(element['value']['id'], '.', element['value'])
         });
-        batchSize += 1000
-
+        current_key += batchSize
     })
-    pipeline.on('end', data => { console.log('finished redis pipeline'); client.disconnect(); return; })
-
-    // await client.disconnect()
-
-    // let objectCounter = 0;
-    // parser.on('data', data => console.log(data));
-    // parser.on('end', () => console.log(`Found ${objectCounter} objects.`));
-
-    // all_cards_request.body.pipe(parser);
-
-    // const all_cards = await all_cards_request.json()
+    all_cards_stream.on('end', () => { console.log('finished redis pipeline') })
+    await client.quit()
 }
