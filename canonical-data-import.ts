bd56f247d9b6b9ffd4c93c9ff20cc7a8ea4d8b61
bears <bears@bears.zone>
2022-09-21 09:52:57 -0500

    add prettier config

--- canonical-data-import.ts
+++ canonical-data-import.ts
@@ -1,40 +1,44 @@
+import fetch from 'make-fetch-happen';
+import streamarray from 'stream-json/streamers/StreamArray.js';
+import Batch from 'stream-json/utils/Batch.js';
+import chain from 'stream-chain';
+import redis from 'redis';
 
-import fetch from 'make-fetch-happen'
-import streamarray from 'stream-json/streamers/StreamArray.js'
-import Batch from 'stream-json/utils/Batch.js'
-import chain from 'stream-chain'
-import redis from 'redis'
-
-main()
+main();
 
 async function main() {
-    const bulk_data_req = await fetch('https://api.scryfall.com/bulk-data')
+	const bulk_data_req = await fetch('https://api.scryfall.com/bulk-data');
 
-    const bulk_data = await bulk_data_req.json()
+	const bulk_data = await bulk_data_req.json();
 
-    // TODO: Catch errors
-    const bulk_data_uri = bulk_data['data'][3]['download_uri']
+	// TODO: Catch errors
+	const bulk_data_uri = bulk_data['data'][3]['download_uri'];
 
-    const batchSize = 1000
+	const batchSize = 1000;
 
-    const all_cards_request = await fetch(bulk_data_uri)
-    const all_cards_stream = chain.chain([all_cards_request.body.pipe(streamarray.withParser()), new Batch({ batchSize: batchSize })])
+	const all_cards_request = await fetch(bulk_data_uri);
+	const all_cards_stream = chain.chain([
+		all_cards_request.body.pipe(streamarray.withParser()),
+		new Batch({ batchSize: batchSize })
+	]);
 
-    const client = redis.createClient({});
-    await client.connect().then(() => {
-        client.on('connect', () => console.log('connected to redis successfully!'));
-        client.on('error', (err) => console.log('Redis Client Error', err));
-    });
+	const client = redis.createClient({});
+	await client.connect().then(() => {
+		client.on('connect', () => console.log('connected to redis successfully!'));
+		client.on('error', (err) => console.log('Redis Client Error', err));
+	});
 
-    var current_key = 0
+	var current_key = 0;
 
-    all_cards_stream.on('data', data => {
-        console.log(current_key + ' processing ' + data[0]['value']['id'])
-        data.forEach(async element => {
-            await client.json.set(element['value']['id'], '.', element['value'])
-        });
-        current_key += batchSize
-    })
-    all_cards_stream.on('end', () => { console.log('finished redis pipeline') })
-    await client.quit()
+	all_cards_stream.on('data', (data) => {
+		console.log(current_key + ' processing ' + data[0]['value']['id']);
+		data.forEach(async (element) => {
+			await client.json.set(element['value']['id'], '.', element['value']);
+		});
+		current_key += batchSize;
+	});
+	all_cards_stream.on('end', () => {
+		console.log('finished redis pipeline');
+	});
+	await client.quit();
 }
--- deploy-schema.ts
+++ deploy-schema.ts
@@ -1,57 +1,60 @@
 import { createClient, SchemaFieldTypes } from 'redis';
 
-main()
+main();
 
 async function main() {
+	const client = createClient({});
+	client.connect();
+	client.on('connect', () => console.log('connected to redis successfully!'));
+	client.on('error', (err) => console.log('Redis Client Error', err));
+	try {
+		await client.ft.dropIndex('idx:cards');
+	} catch (e) {
+		if (e.message === 'Unknown Index name') {
+			console.log('Index deleted, skipped creation.');
+		} else {
+			// Something went wrong, perhaps RediSearch isn't installed...
+			console.error(e);
+			process.exit(1);
+		}
+	}
 
-    const client = createClient({});
-    client.connect();
-    client.on('connect', () => console.log('connected to redis successfully!'));
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    try {
-        await client.ft.dropIndex('idx:cards');
-    } catch (e) {
-        if (e.message === 'Unknown Index name') {
-            console.log('Index deleted, skipped creation.');
-        } else {
-            // Something went wrong, perhaps RediSearch isn't installed...
-            console.error(e);
-            process.exit(1);
-        }
-    }
-
-
-    await client.ft.create('idx:cards',
-        {
-            '$.name': {
-                type: SchemaFieldTypes.TEXT,
-                AS: 'name',
-                SORTABLE: true
-            },
-            '$.type_line': {
-                type: SchemaFieldTypes.TEXT,
-                AS: 'type_line',
-                SORTABLE: true
-            },
-            '$.set': {
-                type: SchemaFieldTypes.TEXT,
-                AS: 'set',
-                SORTABLE: true
-            },
-            '$.lang': {
-                type: SchemaFieldTypes.TEXT,
-                AS: 'lang',
-                SORTABLE: true
-            },
-            '$.set_type': {
-                type: SchemaFieldTypes.TEXT,
-                AS: 'set_type',
-                SORTABLE: true
-            }
-        }, {
-        ON: 'JSON'
-    }).finally(() => {
-        client.quit();
-        return;
-    });
+	await client.ft
+		.create(
+			'idx:cards',
+			{
+				'$.name': {
+					type: SchemaFieldTypes.TEXT,
+					AS: 'name',
+					SORTABLE: true
+				},
+				'$.type_line': {
+					type: SchemaFieldTypes.TEXT,
+					AS: 'type_line',
+					SORTABLE: true
+				},
+				'$.set': {
+					type: SchemaFieldTypes.TEXT,
+					AS: 'set',
+					SORTABLE: true
+				},
+				'$.lang': {
+					type: SchemaFieldTypes.TEXT,
+					AS: 'lang',
+					SORTABLE: true
+				},
+				'$.set_type': {
+					type: SchemaFieldTypes.TEXT,
+					AS: 'set_type',
+					SORTABLE: true
+				}
+			},
+			{
+				ON: 'JSON'
+			}
+		)
+		.finally(() => {
+			client.quit();
+			return;
+		});
 }
